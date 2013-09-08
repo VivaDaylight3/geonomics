8ccfc15c0848cf115e06f90edd9863704bb050b6
Aurilux <primordias@gmail.com>
2013-09-08 10:47:31 -0500

    Incubator should work now

--- src/minecraft/vivadaylight3/myrmecology/common/block/BlockIncubator.java
+++ src/minecraft/vivadaylight3/myrmecology/common/block/BlockIncubator.java
@@ -23,82 +23,60 @@ public class BlockIncubator extends Block {
     private Icon iconSideOn;
     private Icon iconSideOff;
 
-    public BlockIncubator(int par1, String par2Name) {
-	super(par1, Material.wood);
-	setStepSound(Block.soundWoodFootstep);
-	setUnlocalizedName(Register.BLOCK_INCUBATOR_NAME);
-	setCreativeTab(Register.tabMyrmecology);
-	setHardness(1.0F);
-	setResistance(1.0F);
-	name = par2Name;
-	func_111022_d(Myrmecology.MOD_ID_LOWER + name);
+    public BlockIncubator(int id, String name) {
+		super(id, Material.wood);
+		setStepSound(Block.soundWoodFootstep);
+		setUnlocalizedName(Register.BLOCK_INCUBATOR_NAME);
+		setCreativeTab(Register.tabMyrmecology);
+		setHardness(1.0F);
+		setResistance(1.0F);
+		this.name = name;
+		func_111022_d(Myrmecology.MOD_ID_LOWER + this.name);
     }
 
     @Override
     public void registerIcons(IconRegister iconRegister) {
-
-	iconTop = iconRegister.registerIcon(Resources.TEXTURE_PREFIX
-		+ Register.BLOCK_INCUBATOR_NAME + "_Top");
-	iconSideOn = iconRegister.registerIcon(Resources.TEXTURE_PREFIX
-		+ Register.BLOCK_INCUBATOR_NAME + "_Side_On");
-	iconSideOff = iconRegister.registerIcon(Resources.TEXTURE_PREFIX
-		+ Register.BLOCK_INCUBATOR_NAME + "_Side_Off");
-
+		iconTop = iconRegister.registerIcon(Resources.TEXTURE_PREFIX + Register.BLOCK_INCUBATOR_NAME + "_Top");
+		iconSideOn = iconRegister.registerIcon(Resources.TEXTURE_PREFIX + Register.BLOCK_INCUBATOR_NAME + "_Side_On");
+		iconSideOff = iconRegister.registerIcon(Resources.TEXTURE_PREFIX + Register.BLOCK_INCUBATOR_NAME + "_Side_Off");
     }
 
     @Override
     public Icon getIcon(int side, int metadata) {
-
-	if (side == 0 || side == 1) {
-
-	    return iconTop;
-
-	} else {
-
-	    if (this.isPowered) {
-
-		return iconSideOn;
-
-	    } else {
-
-		return iconSideOff;
-
-	    }
-
-	}
-
+		if (side == 0 || side == 1) {
+		    return iconTop;
+		}
+		else {
+		    if (this.isPowered) {
+		    	return iconSideOn;
+		    } 
+		    else {
+		    	return iconSideOff;
+		    }
+		}
     }
 
     @Override
-    public boolean canConnectRedstone(IBlockAccess world, int x, int y, int z,
-	    int side) {
-
-	return true;
-
+    public boolean canConnectRedstone(IBlockAccess world, int x, int y, int z, int side) {
+    	return true;
     }
 
     @Override
-    public boolean onBlockActivated(World world, int x, int y, int z,
-	    EntityPlayer player, int par6, float par7, float par8, float par9) {
-
-	if (!player.isSneaking() && world.isRemote) {
-
-	    player.openGui(Myrmecology.instance, Register.GUI_ID_INCUBATOR,
-		    world, x, y, z);
-
-	    return true;
-
-	}
-
-	return false;
-
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int par6, float par7, float par8, float par9) {
+    	if (world.isRemote) {
+    		return true;
+    	}
+    	else {
+    		TileEntity te = world.getBlockTileEntity(x, y, z);
+    		if (te != null && te instanceof TileEntityIncubator && !player.isSneaking()) {
+    		    player.openGui(Myrmecology.instance, Register.GUI_ID_INCUBATOR, world, x, y, z);
+    		}
+		    return true;
+		}
     }
 
     @Override
     public TileEntity createTileEntity(World world, int metadata) {
-
-	return new TileEntityIncubator();
-
+    	return new TileEntityIncubator();
     }
-
 }
